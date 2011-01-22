bf345e9b1444f6dc53bdc44af01ce28b0f27a268
shimizukawa <devnull@localhost>
2011-01-22 15:01:34 +0900

    remove django support code

--- app/lib/__init__.py
+++ app/lib/__init__.py
@@ -1,8 +1,2 @@
 # -*- coding: utf-8 -*-
 
-import google.appengine.ext.webapp.template # need this before import django
-from django.conf import settings
-settings.INSTALLED_APPS = list(settings.INSTALLED_APPS)
-settings.INSTALLED_APPS.append('lib')
-settings.INSTALLED_APPS = tuple(settings.INSTALLED_APPS)
-
