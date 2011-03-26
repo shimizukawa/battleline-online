57e47d194ff895b18786042b05b50fed0cdcc933
shimizukawa <devnull@localhost>
2011-03-26 16:50:41 +0900

    add logged-in index view test.

--- app/test/functional/test_lobby.py
+++ app/test/functional/test_lobby.py
@@ -3,10 +3,20 @@
 from webtest import TestApp
 from nose.tools import *
 from app.controllers.lobby import application
+from gaefw.test.tools import gae_login
 
 app = TestApp(application())
 
-def test_index():
+
+def test_anonymous_index():
     response = app.get('/lobby')
     assert_true('Googleアカウントでログイン' in response.body)
+    assert_false('新規ゲームを作成' in response.body)
+
+
+def test_loggedin_index():
+    gae_login()
+    response = app.get('/lobby')
+    assert_true('新規ゲームを作成' in response.body)
+    assert_false('Googleアカウントでログイン' in response.body)
 
